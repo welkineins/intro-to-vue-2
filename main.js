5c190afcd245fe1723e9a0b3e0daab3b1f7051f6
Chieh Yu <welkineins@gmail.com>
2021-06-04 15:15:24 +0800

    5. Event Handling

--- main.js
+++ main.js
@@ -5,6 +5,27 @@ var app = new Vue({
         image: '1578366170160_4.png',
         altText: 'hahaha',
         inStock: true,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"]
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: '1578366170160_4.png'
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: '1578366211820_6.png'
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function() {
+            this.cart += 1;
+        },
+        updateProduct: function(variantImage) {
+            this.image = variantImage;
+        }
     }
 });
