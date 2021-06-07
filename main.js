636788fd01240e3fdb795a6d1c18877ab92fd288
Chieh Yu <welkineins@gmail.com>
2021-06-07 13:32:22 +0800

    7. Computed Properties

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: "Vue Mastery",
         product: 'Socks',
-        image: '1578366170160_4.png',
+        selectedVariant: 0,
         altText: 'hahaha',
-        inStock: false,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: '1578366170160_4.png'
+                variantImage: '1578366170160_4.png',
+                variantQuantity: 3
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: '1578366211820_6.png'
+                variantImage: '1578366211820_6.png',
+                variantQuantity: 0
             }
         ],
         cart: 0
@@ -24,8 +26,19 @@ var app = new Vue({
         addToCart: function() {
             this.cart += 1;
         },
-        updateProduct: function(variantImage) {
-            this.image = variantImage;
+        updateProduct: function(index) {
+            this.selectedVariant = index;
+        }
+    },
+    computed: {
+        title: function() {
+            return this.brand + ' ' + this.product;
+        },
+        image: function() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock: function() {
+            return this.variants[this.selectedVariant].variantQuantity;
         }
     }
 });
